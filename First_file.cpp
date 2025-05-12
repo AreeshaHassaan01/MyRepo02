0c039bc7ee4e191edd41e485672f3667fe16e81f
AreeshaHassaan01 <universityworkonly02@gmail.com>
2025-05-13 00:30:01 +0500

    Update First_file.cpp

--- First_file.cpp
+++ First_file.cpp
@@ -5,3 +5,8 @@
        cout << "Roll Number: 24-ST-034" << endl;  
        return 0;  
    }
+
+ cout << "FSC Subjects:" << endl;  
+   cout << "1. Physics" << endl;  
+   cout << "2. Chemistry" << endl;  
+   cout << "3. Mathematics" << endl;
