5fd950ecc3b1ba007af6c469a072264a62e48a1f
AreeshaHassaan01 <universityworkonly02@gmail.com>
2025-05-13 00:16:57 +0500

    Create First_file.cpp

---
+++ First_file.cpp
@@ -0,0 +1,7 @@
+ #include <iostream>  
+   using namespace std;  
+   int main() {  
+       cout << "Areesha Hassaan" << endl;  
+       cout << "Roll Number: 24-ST-034" << endl;  
+       return 0;  
+   }
